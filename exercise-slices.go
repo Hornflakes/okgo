f385a812fee4e3a58ea62f97226026777d21212d
Hornflakes <cristianpetrugug@gmail.com>
2026-04-01 17:03:56 +0300

    feat: slice and dice

---
+++ exercise-slices.go
@@ -0,0 +1,44 @@
+// https://go.dev/tour/moretypes/18
+
+package main
+
+import (
+	"math/rand"
+
+	"golang.org/x/tour/pic"
+)
+
+func one(x, y int) uint8 {
+	return uint8((x + y) / 2)
+}
+func two(x, y int) uint8 {
+	return uint8(x * y)
+}
+func three(x, y int) uint8 {
+	return uint8(x ^ y)
+}
+
+var funcs = []func(int, int) uint8{
+	one,
+	two,
+	three,
+}
+
+func Pic(dx, dy int) (pic [][]uint8) {
+	pic = make([][]uint8, dy)
+	f := funcs[rand.Intn(3)]
+
+	for y := 0; y < dy; y++ {
+		pic[y] = make([]uint8, dx)
+
+		for x := 0; x < dx; x++ {
+			pic[y][x] = f(x, y)
+		}
+	}
+
+	return
+}
+
+func main() {
+	pic.Show(Pic)
+}
